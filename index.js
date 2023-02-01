c419a7ee7c9b1d1f1441e06db9cfa679f5ade28b
RXTArtemis <r_nava20@outlook.com>
2023-02-01 11:39:12 -0600

    other changes

--- index.js
+++ index.js
@@ -1,70 +1,164 @@
-const suits= ['Clubs','Hearts','Diamonds','Spades']
-const rank=['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-const deck=[];
-const cardData=[];
-const card=[];
-const player1Cards=[];
-const player2Cards=[];
-p1Score=0;
-p2Score=0;
+let ranks=['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+let suits=['Clubs','Hearts','Diamonds','Spades'];
+let cardData=[];
+let deck=[]
+let card=[];
+let player1Cards=[];
+let player2Cards=[];
+let p1Score=0;
+let p2Score=0;
 
-function buildDeck(){
-    cardData.suits.forEach((suit)=>{
-        cardData.values.forEach((value)=>{
-            const card={
-                suits:suits,
-                cardNumber:value,
-                cardValue:index+1
-            }
-            deck.push(card);
-        })
-    })
-    deck.sort(()=>{
-        return Math.random()-0.5;
-    })
+
+//player class
+class Player{
+    constructor(player1,player2){
+        this.player1=player1;
+        this.player2=player2;
+    }
+    
 }
+//game introduction
+console.log("Welcome Players. Lets play War.");
 
-function shuffleDeck(deck){
-    for(let i=0; i<52;i++){
-        const tempCard=deck[i];
-        const index = Math.floor(Math.random()*52);
-        deck[i]=deck[index];
-        deck[index] = tempCard;
+//card class
+class Card{
+    constructor(suits,ranks){
+        this.suits=suits;
+        this.ranks=ranks;
     }
 }
+console.log(suits);
+console.log(ranks);
 
-function cardPlay(){
+//building deck
+function buildDeck(){
+ for(let suitCounter=0; suitCounter <4; suitCounter++){
+    for(let rankCounter=0; rankCounter<13; rankCounter++){
+        deck.push(ranks[rankCounter]+suits[suitCounter]);
+        console.log(ranks[rankCounter]+suits[suitCounter]);
+    }
+ }
+ return deck;
+}
+console.log(deck);
+
+//shuffling deck
+
+function shuffleDeck(deck){
+    for(let i=deck.length -1;i>0;i--){
+        let x=Math.floor(Math.random()*(i+1));
+        [deck[i],deck[x]]-[deck[x],deck[i]];
+    }
+
+ }
+
+let testDeck= buildDeck();
+shuffleDeck(testDeck);
+console.log(testDeck);
+
 for(let i=0; i<26;i++){
     player1Cards.push(deck[i]);
     player2Cards.push(deck[i+26]);
-}if(player1Cards > player2Cards){
-    player1Cards.unshift(player2Cards);
-}else if(player1Cards<player2Cards){
-    player2Cards.unshift(player1Cards);
+
+    console.log(player1Cards);
+
+
+   console.log(player2Cards);
+    
+   function cardPlay(){
+    if(player1Cards > player2Cards){
+        p1Score+1;
+    }else if(player1Cards<player2Cards){
+        p2Score+1;
+    }
+  }
+//created a game class similar to menu..
+ let player1Plays=cardPlay(player2Cards);
+let player2Plays=cardPlay(player1Cards);
+let round=compareCards(player1Cards,player2Cards);
+
+class Play{
+constructor(){
+    this.player1=player1;
+    this.player2=player2;
+} describe(){
+
+}compareCards(){
+}cardPlay(){
+
+}
+}if(round===1){
+    p1Score++;
+}else(round===-1)
+    p2Score++;
+}
+declareWinner();
+
+function compareCards(secondCard){
+    if(this.rank>secondCard.rank){
+        return +1;
+    }else if(this.rank<secondCard.rank){
+        return -1;
+    }else if(this.rank==secondCard.rank){
+        return 0;
     }
 }
+
+function moveCard(){
+    if(0<player1Cards && 0<player2Cards){
+       for(let i=0; i<26;i--){
+             player1Cards.shift();
+             player2Cards.shift();
+        }if(player1Cards[i]<player2Cards[i]){
+            player1Cards.shift(playedCard);
+            player2Cards.unshift(playedCard);
+        }else if(player1Cards[i]>player2Cards[i]){
+            player2Cards.shift(playedCard);
+            player1Cards.unshift(playedCard);
+            }
+         }
+     }
+
 
 function scoreBox(){
     for(let i=0; i<26;i++){
-        if(player1Cards>player2Cards){
-            p1Score++;
-        }else(player1Cards<player2Cards){
-            p2Score++;
-    
+        if(Math.max(player1Cards[i]>player2Cards[i])){
+            player1Cards.push(p1Score);
+            p1Score+1;
+        }else if(Math.max(player1Cards[i]<player2Cards[i])){
+            player2Cards.push(p2Score);
+            p2Score+1;
         }
+      }
+    
     }
-}
 
-function Loser(){
-    if(player1Cards===0){
+function declareWinner(){
+    if(p1Score>p2Score){
+        console.log("Winner is Player 1!")
+    }else if(p1Score<p2Score){
+        console.log("Winner is Player 2!")
+    }else(p1Score === p2Score)
+    console.log("Draw. We have a tie.")
+    }
+    console.log("Player 1 score:"+ p1Score);
+    console.log("Player 2 score:"+ p2Score);
+
+
+
+function loser(){
+    if(player1Cards===0 && player2Cards>0){
         console.log("Player 1 lost. Winner is Player 2!");
-    }else(player2Cards===0){
+    }else if(player2Cards===0 && player1Cards>0){
         console.log("Player 2 lost. Winner is Player 1!")
     }
 }
 
-buildDeck();
-shuffleDeck();
-cardPlay();
-scoreBox();
-Loser();
+
+
+
+//buildDeck();
+//shuffleDeck();
+//cardPlay();
+//scoreBox();
+//loser();
---
+++ index_test.js
@@ -0,0 +1,11 @@
+
+let  expect =chai.expect;
+
+describe('MyFunctions',function(){
+    describe('buildDeck',function(){
+        it('should put 52 cards in deck', function(){
+            let deck=buildDeck();
+            expect(deck).to.have.lengthOf(52);
+        })
+    });
+});
